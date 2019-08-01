9642489af2130cef33b2fd739d09f7b2772e5afc
Nicholas Lambourne <dev@ndl.au>
2019-08-01 21:45:07 +1000

    added attachment and message resolution

---
+++ slackblocks/attachments.py
@@ -0,0 +1,47 @@
+from enum import Enum
+from json import dumps
+from typing import Any, Dict, List, Optional, Union
+from .blocks import Block
+from .error import InvalidUsageError
+
+
+class Color(Enum):
+    GOOD = "good"
+    WARNING = "warning"
+    DANGER = "danger"
+    RED = "#ff0000"
+    BLUE = "#0000ff"
+    YELLOW = "#ffff00"
+    GREEN = "#00ff00"
+    ORANGE = "#ff8800"
+    PURPLE = "#8800ff"
+    BLACK = "#000000"
+
+
+class Attachment:
+    """
+    Secondary content can be attached to messages to include lower priority content
+     - content that doesn't necessarily need to be seen to appreciate the intent of
+     the message, but perhaps adds further context or additional information.
+    """
+    def __init__(self,
+                 blocks: Optional[List[Block]],
+                 color: Optional[Union[str, Color]]):
+        self.blocks = blocks
+        if type(color) is Color:
+            self.color = color.value
+        elif type(color) is str:
+            if len(color) == 7 and color[0] == "#":
+                self.color = color
+            else:
+                raise InvalidUsageError("Color must be a valid hex code (e.g. #ffffff)")
+
+    def _resolve(self) -> Dict[str, Any]:
+        attachment = dict()
+        attachment["blocks"] = [block._resolve() for block in self.blocks]
+        return attachment
+
+    def __repr__(self) -> str:
+        return dumps(self._resolve(), indent=4)
+
+
