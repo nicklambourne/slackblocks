876057e66f892579a26e94e4feed7f2d8d26e263
Nicholas Lambourne <dev@ndl.au>
2019-08-02 17:43:52 +1000

    added manifest

--- slackblocks/__init__.py
+++ slackblocks/__init__.py
@@ -1,7 +1,7 @@
 from .attachments import Attachment, Color, Field
 from .blocks import ActionsBlock, ContextBlock, DividerBlock, FileBlock, \
     ImageBlock, SectionBlock
-from .elements import Element, Image, Text, TextType
+from .elements import Button, Confirm, Element, Image, Text, TextType
 from .messages import Message
 
 name = "slackblocks"
