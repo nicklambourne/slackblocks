7f94322664b9e487cd9073e8dfde3f26794a878b
nicklambourne <ndl93@live.com>
2019-08-03 14:02:40 +1000

    full readme and image

--- slackblocks/messages.py
+++ slackblocks/messages.py
@@ -10,11 +10,13 @@ class Message:
     the Slack message API.
     """
     def __init__(self,
+                 channel: str,
                  text: Optional[str] = None,
                  blocks: Optional[Union[List[Block], Block]] = None,
                  attachments: Optional[List[Attachment]] = None,
                  thread_ts: Optional[str] = None,
                  mrkdwn: bool = True):
+        self.channel = channel
         if isinstance(blocks, List):
             self.blocks = blocks
         elif isinstance(blocks, Block):
@@ -28,6 +30,7 @@ class Message:
 
     def _resolve(self) -> Dict[str, Any]:
         message = dict()
+        message["channel"] = self.channel
         message["mrkdwn"] = self.mrkdwn
         if self.blocks:
             message["blocks"] = [block._resolve() for block in self.blocks]
@@ -44,3 +47,11 @@ class Message:
 
     def __repr__(self) -> str:
         return self.json()
+
+    def __getitem__(self, item):
+        return self._resolve()[item]
+
+    def keys(self) -> Dict[str, Any]:
+        return self._resolve()
+
+
