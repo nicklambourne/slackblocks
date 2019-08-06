f0dc4d32bbea6bde0f2be0d92f953473522b5c3c
Nicholas Lambourne <dev@ndl.au>
2019-08-06 23:28:21 +1000

    more tests

--- test/test_messages.py
+++ test/test_messages.py
@@ -1,8 +1,16 @@
-import pytest
-from slackblocks import Message, SectionBlock
+from slackblocks import Attachment, Color, Message, SectionBlock
 
 
-def test_basic_messaget() -> None:
-    x = SectionBlock("Hello, world!")
-    m = Message(channel="#general", blocks=x)
-    assert 1 == 1
+def test_basic_message() -> None:
+    block = SectionBlock("Hello, world!", block_id="fake_block_id")
+    message = Message(channel="#general", blocks=block)
+    with open("test/samples/message_simple.json", "r") as expected:
+        assert repr(message) == expected.read()
+
+
+def test_message_with_attachment() -> None:
+    block = SectionBlock("Hello, world!", block_id="fake_block_id")
+    attachment = Attachment(blocks=block, color=Color.YELLOW)
+    message = Message(channel="#general", attachments=[attachment, ])
+    with open("test/samples/message_with_attachments.json", "r") as expected:
+        assert repr(message) == expected.read()
