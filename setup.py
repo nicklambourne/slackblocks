84f5f81f751b831da9563791a4c8b59751190a1b
Nicholas Lambourne <dev@ndl.au>
2020-08-13 23:27:14 +1000

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ production_requirements = []
 
 setup(
     name="slackblocks",
-    version="0.1.8",
+    version="0.2.1",
     author="Nicholas Lambourne",
     author_email="nick@ndl.im",
     description="Python wrapper for the Slack Blocks API",
