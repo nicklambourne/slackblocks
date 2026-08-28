eef7235e7262ab00b04ec82d2bf66c076a5bb6bd
Nicholas Lambourne <dev@ndl.au>
2026-08-28 23:37:41 +1000

    feat(typescript): add fluent payload builders

--- typescript/src/fluent/index.ts
+++ typescript/src/fluent/index.ts
@@ -3,3 +3,4 @@ export type { Buildable, FluentBuilder } from "./core.js";
 export * from "./blocks.js";
 export * from "./elements.js";
 export * from "./objects.js";
+export * from "./payloads.js";
